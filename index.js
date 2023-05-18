6f2ebf8b4c2cb9d3a67103f8e80b356c04b0924f
asefibrahim <strongman01666@gmail.com>
2023-05-18 21:53:14 +0600

    Loaded category data

--- index.js
+++ index.js
@@ -27,11 +27,19 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
+        const categoryCollection = client.db('categoryDB').collection('category')
+
+        app.get('/categories', async (req, res) => {
+            const result = await categoryCollection.find().toArray()
+            res.send(result)
+        })
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
